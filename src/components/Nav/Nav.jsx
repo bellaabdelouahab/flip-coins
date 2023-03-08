8d41c4f96894ddc0fd7ae0782d741ecaa8e31903
= <=>
2023-03-09 00:56:32 +0100

    pull 2

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './nav.css'
+import coin_pic from '../../assets/cube_score.png'
 
 export default function Nav () {
+  const [score, setScore] = useState(0);
+  
   return (
     <div className='nav_area'>
         <div className='nv-options'>
           <button className='Add-hand'> Buy One More Hand</button>
           </div>
         <div className='coins'>
-          {/* div for  */}
+          <div className='score'>
+              <img src={coin_pic} alt='coin' className='coin_pic' />
+              <span className='coin_count'>{score}</span>
+          </div>
+
+          <div className='settings'>
+            <span className="material-icons-sharp">settings</span>
+          </div>
         </div>
     </div>
   )
